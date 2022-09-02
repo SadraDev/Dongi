91b2bd25af446a9b3da11aea7a520565f7130546
Sadra Galavani <sadra.galavani78@gmail.com>
2022-09-02 18:46:04 +0430

    separated UI and widgets

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,7 +1,7 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
-
 import 'package:flutter/material.dart';
-import 'package:my_hesab_ketab/constants.dart';
+import 'package:my_hesab_ketab/ui_widgets/profile_screen/Friends.dart';
+import 'package:my_hesab_ketab/ui_widgets/profile_screen/balance_info.dart';
+import 'package:my_hesab_ketab/ui_widgets/profile_screen/profile_info.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -13,261 +13,38 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    final dark = MediaQuery.of(context).platformBrightness == Brightness.dark;
     return ListView(
-      padding: EdgeInsets.only(bottom: 100),
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: 100.0, horizontal: MediaQuery.of(context).size.width / 3)
-              .copyWith(bottom: 0),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image.asset(
-              'assets/images/profile.jpg',
-              fit: BoxFit.cover,
-            ),
-          ),
+      padding: const EdgeInsets.only(bottom: 100),
+      children: const <Widget>[
+        ProfileImage(profileImg: 'profile.jpg'),
+        ProfileUsername(username: 'SadraDev'),
+        ProfileBalanceInformation(
+          payedAmount: '410,000',
+          earnedAmount: '620,000',
+          pendingBalance: '210,000',
+          pendingBalanceValue: false,
         ),
-        Padding(
-          padding: const EdgeInsets.only(top: 12.0, bottom: 35),
-          child: Text(
-            'SadraDev',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: dark ? kWhite : kBlack,
-              fontSize: 30,
-            ),
-          ),
+        ProfileFriendsText(),
+        ProfileFriendBubble(
+          friendProfileImg: 'profile.jpg',
+          friendUsername: 'Mohamad',
+          addedDate: '2022 06 16',
+          amount: '60000',
+          amountValue: false,
         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12.0),
-          child: Card(
-            color: !dark ? kLighterGrey : kGrey, //TODO
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Text(
-                    'Total Balance History',
-                    style: TextStyle(color: kWhite, fontSize: 24),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 12.0),
-                      child: Text(
-                        '220,000',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: kRed, fontSize: 30),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 12.0),
-                      child: Text(
-                        '440,000',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: kGreen, fontSize: 30),
-                      ),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Text(
-                    'Pending Balance',
-                    style: TextStyle(color: kWhite, fontSize: 24),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 12.0),
-                  child: Text(
-                    '220,000',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: kRed, fontSize: 30),
-                  ),
-                ),
-              ],
-            ),
-          ),
+        ProfileFriendBubble(
+          friendProfileImg: 'profile.jpg',
+          friendUsername: 'Amir',
+          addedDate: '2022 06 16',
+          amount: '70000',
+          amountValue: false,
         ),
-        SizedBox(
-          width: MediaQuery.of(context).size.width - 24,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 24),
-            child: Text(
-              'Friends',
-              style: TextStyle(
-                color: dark ? kWhite : kBlack,
-                fontSize: 24,
-              ),
-            ),
-          ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24).copyWith(top: 5.0),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(12),
-                    child: Image.asset(
-                      'assets/images/profile.jpg',
-                      height: 50,
-                      width: 50,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'Mohamad',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: dark ? kWhite : kBlack,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 4.0),
-                      child: Text(
-                        'added at 2022 06 16',
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: kLighterGrey,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0).copyWith(top: 5.0),
-              child: Text(
-                '60000',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: kRed,
-                ),
-              ),
-            )
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24).copyWith(top: 5.0),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(12),
-                    child: Image.asset(
-                      'assets/images/profile.jpg',
-                      height: 50,
-                      width: 50,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'AmirMmd',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: dark ? kWhite : kBlack,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 4.0),
-                      child: Text(
-                        'added at 2022 06 16',
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: kLighterGrey,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0).copyWith(top: 5.0),
-              child: Text(
-                '70000',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: kRed,
-                ),
-              ),
-            )
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24).copyWith(top: 5.0),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(12),
-                    child: Image.asset(
-                      'assets/images/profile.jpg',
-                      height: 50,
-                      width: 50,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'AmirHossein',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: dark ? kWhite : kBlack,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 4.0),
-                      child: Text(
-                        'added at 2022 06 16',
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: kLighterGrey,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0).copyWith(top: 5.0),
-              child: Text(
-                '90000',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: kRed,
-                ),
-              ),
-            )
-          ],
+        ProfileFriendBubble(
+          friendProfileImg: 'profile.jpg',
+          friendUsername: 'AmirHossein',
+          addedDate: '2022 06 16',
+          amount: '80000',
+          amountValue: false,
         ),
       ],
     );
