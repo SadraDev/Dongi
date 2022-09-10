dee647068b94de2bf9add8728849ebec1f8fedee
Sadra Galavani <sadra.galavani78@gmail.com>
2022-09-10 18:07:44 +0430

    added addFriend screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:my_hesab_ketab/ui_widgets/profile_screen/Friends.dart';
+import 'package:my_hesab_ketab/ui_widgets/profile_screen/add_friend.dart';
 import 'package:my_hesab_ketab/ui_widgets/profile_screen/balance_info.dart';
 import 'package:my_hesab_ketab/ui_widgets/profile_screen/profile_info.dart';
 
@@ -15,35 +17,37 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return ListView(
       padding: const EdgeInsets.only(bottom: 100),
-      children: const <Widget>[
-        ProfileImage(profileImg: 'profile.jpg'),
-        ProfileUsername(username: 'SadraDev'),
-        ProfileBalanceInformation(
+      children: <Widget>[
+        const ProfileImage(profileImg: 'profile.jpg'),
+        const ProfileUsername(username: 'SadraDev'),
+        const ProfileBalanceInformation(
           payedAmount: '410,000',
-          earnedAmount: '620,000',
-          pendingBalance: '210,000',
+          earnedAmount: '665,000',
+          pendingBalance: '255,000',
           pendingBalanceValue: false,
         ),
-        ProfileFriendsText(),
-        ProfileFriendBubble(
+        ProfileFriendsText(
+          onAddFriendBuilder: (context) => const ProfileAddFriendScreen(),
+        ),
+        const ProfileFriendBubble(
           friendProfileImg: 'profile.jpg',
           friendUsername: 'Mohamad',
           addedDate: '2022 06 16',
           amount: '60000',
           amountValue: false,
         ),
-        ProfileFriendBubble(
+        const ProfileFriendBubble(
           friendProfileImg: 'profile.jpg',
           friendUsername: 'Amir',
           addedDate: '2022 06 16',
           amount: '70000',
           amountValue: false,
         ),
-        ProfileFriendBubble(
+        const ProfileFriendBubble(
           friendProfileImg: 'profile.jpg',
           friendUsername: 'AmirHossein',
           addedDate: '2022 06 16',
-          amount: '80000',
+          amount: '125000',
           amountValue: false,
         ),
       ],
