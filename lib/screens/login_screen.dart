bd0e70f3029910544c88008aaa74f8798468a64b
Sadra Galavani <sadra.galavani78@gmail.com>
2022-09-12 02:48:27 +0430

    functioning login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:my_hesab_ketab/screens/flow_screen.dart';
+import 'package:my_hesab_ketab/screens/utilities/api.dart';
+import 'package:my_hesab_ketab/screens/utilities/shared.dart';
 import 'package:my_hesab_ketab/ui_widgets/login_screen/buttons.dart';
 import 'package:my_hesab_ketab/ui_widgets/login_screen/textfields.dart';
 
@@ -16,19 +19,59 @@ class _LoginScreenState extends State<LoginScreen> {
   String? _username;
   String? _password;
 
+  login() => Navigator.popAndPushNamed(context, FlowScreen.id);
+
   @override
   Widget build(BuildContext context) {
     return LoginScaffold(
       children: <Widget>[
         LoginTextFields(
-          onUsernameSubmitted: (value) => _username = value,
-          onPasswordSubmitted: (value) => _password = value,
+          onUsernameChanged: (value) => _username = value,
+          onPasswordChanged: (value) => _password = value,
         ),
         LoginButtons(
           loginLoading: loginLoading,
           registerLoading: registerLoading,
-          onLogin: () {},
-          onRegister: () {},
+          onLogin: () async {
+            setState(() => loginLoading = true);
+            String logged = await Api.login(_username!, _password!);
+            setState(() => loginLoading = false);
+            if (logged == 'true') {
+              Shared.setUserName(_username!);
+              Shared.setUserPassword(_password!);
+              login();
+            } else {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  content: Text(
+                    logged,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              );
+            }
+          },
+          onRegister: () async {
+            setState(() => registerLoading = true);
+            String logged = await Api.register(_username!, _password!);
+            setState(() => registerLoading = false);
+            if (logged == 'true') {
+              Shared.setUserName(_username!);
+              Shared.setUserPassword(_password!);
+              login();
+            } else {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  content: Text(
+                    logged,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              );
+            }
+          },
         ),
       ],
     );
