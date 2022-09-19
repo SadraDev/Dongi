e271ab19e48905b3901a3643d9afc696bc7ebb82
Sadra Galavani <sadra.galavani78@gmail.com>
2022-09-19 04:35:11 +0430

    added new routes

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:my_hesab_ketab/screens/login_screen.dart';
 import 'package:my_hesab_ketab/screens/profile_screen.dart';
 import 'package:my_hesab_ketab/screens/splash_screen.dart';
 import 'package:my_hesab_ketab/screens/utilities/shared.dart';
+import 'package:my_hesab_ketab/ui_widgets/add_screen/add_cat.dart';
+import 'package:my_hesab_ketab/ui_widgets/home_screen/notifications.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,7 +30,9 @@ class MyApp extends StatelessWidget {
         AddScreen.id: (context) => const AddScreen(),
         ProfileScreen.id: (context) => const ProfileScreen(),
         FlowScreen.id: (context) => const FlowScreen(),
-        SplashScreen.id: (context) => const SplashScreen()
+        SplashScreen.id: (context) => const SplashScreen(),
+        AddAddCategoryScreen.id: (context) => const AddAddCategoryScreen(),
+        HomeNotificationsScreen.id: (context) => const HomeNotificationsScreen(),
       },
     );
   }
